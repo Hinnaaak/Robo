7d75351b8359bab2b4a65ec57f6a424d16c00b55
Rofelmau <lucaschueller@web.de>
2018-10-31 23:45:11 +0100

    Added Everything

---
+++ StoryTelling/StoryTelling.pml
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="StoryTelling" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="choice_sentences" src="behavior_1/Aldebaran/choice_sentences.xml" />
+        <File name="index" src="html/index.html" />
+        <File name="jquery.min" src="html/jquery.min.js" />
+        <File name="robotutils" src="html/robotutils.js" />
+        <File name="touch" src="html/touch.js" />
+        <File name="index" src="html/de/index.html" />
+        <File name="index" src="html/en/index.html" />
+        <File name="index2" src="html/de/index2.html" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ StoryTelling/translations/translation_en_US.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Auswahl (1)/Say Global</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Welche Geschichte möchtest du hören?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Welche Geschichte möchtest du hören?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Auswahl/Say Global</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Möchtest du eine Geschichte hören?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Möchtest du eine Geschichte hören?</translation>
+        </message>
+    </context>
+</TS>
